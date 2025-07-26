1d7aacbd574fc242000e287a16457fcf835e9534
Sascha Nesterovic <nesterovic5@gmail.com>
2025-07-27 01:22:48 +0200

    I hate symlinks

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,1 +1,5 @@
-../../common/os_pickup_audio-ar.mk
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+PRODUCT_PACKAGES -= vendor.qti.hardware.AGMIPC@1.0-impl
+PRODUCT_PACKAGES -= liblx-osal
+include $(call first-makefiles-under,$(call my-dir))
+endif
